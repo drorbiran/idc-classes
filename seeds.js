7ef99801f68c88c5be56534e6f4dd439f6548a93
Dror Biran <drorb@wix.com>
2017-08-09 11:11:06 +0300

    styling the course page

--- seeds.js
+++ seeds.js
@@ -7,17 +7,17 @@ var data = [
     {
         name: "Course1",
         image: "http://placehold.it/160x160&text=course1",
-        description: "This is the description for this course"
+        description: "This is the description for this course This is the description for this course This is the description for this course This is the description for this course This is the description for this course"
     },
     {
         name: "Course2",
         image: "http://placehold.it/160x160&text=course2",
-        description: "This is the description for this course"
+        description: "This is the description for this course This is the description for this course This is the description for this course This is the description for this course This is the description for this course"
     },
     {
         name: "Course3",
         image: "http://placehold.it/160x160&text=course3",
-        description: "This is the description for this course"
+        description: "This is the description for this course This is the description for this course This is the description for this course This is the description for this course This is the description for this course"
     }
 ];
 
