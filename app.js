5e73c3a5bce2ec2d03755e1b65af9c7f35a26406
Dror Biran <drorb@wix.com>
2017-08-09 12:21:52 +0300

    adding a main.css file and styling the images css

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ mongoose.connect("mongodb://localhost/idc", {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", function(req, res){
