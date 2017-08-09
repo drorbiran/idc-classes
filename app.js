55d07d94c47b493905417cd53f974425f916a2fb
Dror Biran <drorb@wix.com>
2017-08-09 13:32:07 +0300

    adding register page

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var Course      = require("./models/course"),
 
 seedDB();
 
+//mongoose config
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/idc", {
     useMongoClient: true
@@ -25,10 +26,22 @@ mongoose.connect("mongodb://localhost/idc", {
     console.log(err);
 });
 
+//general config
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+//passport config
+app.use(require("express-session")({
+    secret: "Hakol beseder todo bom",
+    resave: false,
+    saveUninitialized  : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
    res.render("landing");
@@ -125,6 +138,28 @@ app.post("/courses/:id/comments", function(req,res){
     });
 });
 
+//=============================
+// AUTH ROUTES
+//=============================
+
+//SHOW - show the register form
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+//handle register logic
+app.post("/register", function (req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err) {
+            console.log(err);
+            return res.render("/register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/courses");
+        });
+    });
+});
 
 
 app.listen(3000,function(){
