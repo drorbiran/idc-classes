119af0a01d9f6d881e1ac46dbd4ec63f8bea796a
Dror Biran <drorb@wix.com>
2017-08-09 12:48:30 +0300

    adding user model

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-var express     = require("express");
-    app         = express();
-    bodyParser  = require('body-parser');
-    mongoose    = require("mongoose");
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require('body-parser'),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local");
+
+
+
+//models
+var Course      = require("./models/course"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
     seedDB      = require("./seeds.js");
 
 seedDB();
-
-//models
-var Course      = require("./models/course");
-    Comment     = require("./models/comment");
-
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/idc", {
